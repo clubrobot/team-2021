b978a1de99232884ed3e471f5de0a86ce1b8d20d
Mathis Lécrivain <matlecr35@gmail.com>
2020-09-23 00:57:21 +0200

    fix beacon setup error
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/setups/setup_beacons.py
+++ raspberrypi/setups/setup_beacons.py
@@ -6,8 +6,9 @@ from setups.setup_wheeledbase import *
 from beacons.robot_client import *
 
 try:
-    robot_beacon = RobotClient(ROBOT_ID, wheeledbase.get_position,ip='127.0.0.1')
+    robot_beacon = RobotClient(
+        ROBOT_ID, wheeledbase.get_position, ip='127.0.0.1')
     robot_beacon.connect()
     robot_beacon.reset_ressources()
-except TimeoutError:
+except (TimeoutError, OSError):
     pass
