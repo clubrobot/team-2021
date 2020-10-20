f8ea4bc378605813bdd08bf28f084cdc419453f8
Mathis Lécrivain <matlecr35@gmail.com>
2020-10-21 00:36:53 +0200

    revert shift register
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/common/ShiftRegister/ShiftRegister.cpp
+++ esp32/common/ShiftRegister/ShiftRegister.cpp
@@ -1,6 +1,34 @@
 #include "ShiftRegister.h"
 #include <Arduino.h>
 
+//global instace
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, byte val)
+{
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        if (bitOrder == LSBFIRST)
+        {
+            digitalWrite(dataPin, !!(val & (1 << i)));
+            delayMicroseconds(45);
+        }
+        else
+        {
+            digitalWrite(dataPin, !!(val & (1 << (7 - i))));
+            delayMicroseconds(45);
+        }
+
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(45);
+        digitalWrite(clockPin, LOW);
+        delayMicroseconds(45);
+    }
+}
+
+//ShiftRegister shift;
+
 void ShiftRegister::attach(uint8_t latchpin, uint8_t clockpin, uint8_t datapin)
 {
     m_LATCH = latchpin;
@@ -11,19 +39,12 @@ void ShiftRegister::attach(uint8_t latchpin, uint8_t clockpin, uint8_t datapin)
     pinMode(m_CLOCK, OUTPUT);
     pinMode(m_DATA, OUTPUT);
 
-    digitalWrite(m_LATCH, 0);
-    digitalWrite(m_CLOCK, 0);
-    digitalWrite(m_DATA, 0);
-
     m_register = 0;
-
-    // Clear on init
-    clear();
 }
 
 void ShiftRegister::SetHigh(int pos)
 {
-    if (pos < m_size && pos >= 0)
+    if (pos <= 7 && pos >= 0)
     {
         m_register |= (1 << pos);
         shift();
@@ -32,7 +53,7 @@ void ShiftRegister::SetHigh(int pos)
 
 void ShiftRegister::SetLow(int pos)
 {
-    if (pos < m_size && pos >= 0)
+    if (pos <= 7 && pos >= 0)
     {
         m_register &= ~(1 << pos);
         shift();
@@ -41,38 +62,9 @@ void ShiftRegister::SetLow(int pos)
 
 void ShiftRegister::shift()
 {
-    if (m_size > 0)
-    {
-        digitalWrite(m_LATCH, LOW);
-        for (int i = 0; i < m_size; i++)
-        {
-            digitalWrite(m_DATA, !!(m_register & (1 << i)));
-            step();
-        }
-        digitalWrite(m_LATCH, HIGH);
-    }
-}
-
-void ShiftRegister::clear()
-{
-    if (m_size > 0)
-    {
-        digitalWrite(m_LATCH, LOW);
-        for (int i = 0; i < m_size; i++)
-        {
-            digitalWrite(m_DATA, !!(m_register & (1 << ((m_size - 1) - i))));
-            step();
-        }
-        digitalWrite(m_LATCH, HIGH);
-    }
-}
-
-void ShiftRegister::step()
-{
-    delayMicroseconds(45);
-    digitalWrite(m_CLOCK, HIGH);
-    delayMicroseconds(45);
-    digitalWrite(m_CLOCK, LOW);
+    digitalWrite(m_LATCH, LOW);
+    shiftOut(m_DATA, m_CLOCK, MSBFIRST, m_register);
+    digitalWrite(m_LATCH, HIGH);
 }
 
 void ShiftRegister::write(int pos, int state)
