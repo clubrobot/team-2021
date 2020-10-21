543004d2b03d96e59d671b9a3219f97d07c7ffef
Mathis Lécrivain <mlecrivain@esoftthings.com>
2020-10-21 13:23:45 +0200

    improve sensors.py

--- raspberrypi/daughter_cards/sensors.py
+++ raspberrypi/daughter_cards/sensors.py
@@ -5,6 +5,8 @@ from common.serialutils import Deserializer
 from daughter_cards.arduino import SecureArduino, TopicHandler, USHORT, BYTE
 import time
 
+UNUSED_SENSOR = 65535
+
 # Instructions
 GET_SENSOR1_OPCODE = 0x10
 GET_SENSOR2_OPCODE = 0x11
@@ -22,7 +24,7 @@ GET_ALL_TOPIC_OPCODE = 0x01
 
 class Sensors(SecureArduino):
     TIMESTEP = 200
-    MAX_DIST = 1000
+    MAX_DIST = 5000
     DEFAULT = {GET_SENSOR1_OPCODE: Deserializer(USHORT(MAX_DIST)),
                GET_SENSOR2_OPCODE: Deserializer(USHORT(MAX_DIST)),
                GET_SENSOR3_OPCODE: Deserializer(USHORT(MAX_DIST)),
@@ -36,8 +38,11 @@ class Sensors(SecureArduino):
         SecureArduino.__init__(self, parent, uuid, default_result=self.DEFAULT)
         self.last_time = None
 
-        self.addTopic(GET_ALL_TOPIC_OPCODE,
+        try:
+            self.addTopic(GET_ALL_TOPIC_OPCODE,
                       self.get_all_sensors_handler, "sensors", self.TIMESTEP)
+        except:
+            pass
 
         self.sensor1 = self.MAX_DIST
         self.sensor2 = self.MAX_DIST
@@ -62,29 +67,53 @@ class Sensors(SecureArduino):
     def get_all(self):
         return [self.sensor1, self.sensor2, self.sensor3, self.sensor4, self.sensor5, self.sensor6, self.sensor7, self.sensor8]
 
-    def get_range1(self):
-        return self.sensor1, 65535
+    def get_range_left_front(self):
+        return self.sensor1, UNUSED_SENSOR
 
-    def get_range2(self):
-        return self.sensor2, 65535
+    def get_range_mid_left_front(self):
+        return self.sensor2, UNUSED_SENSOR
 
-    def get_range3(self):
-        return self.sensor3, 65535
+    def get_range_mid_right_front(self):
+        return self.sensor3, UNUSED_SENSOR
 
-    def get_range4(self):
-        return self.sensor4, 65535
+    def get_range_right_front(self):
+        return self.sensor4, UNUSED_SENSOR
 
-    def get_range5(self):
-        return self.sensor5, 65535
+    def get_range_left_back(self):
+        return self.sensor5, UNUSED_SENSOR
 
-    def get_range6(self):
-        return self.sensor6, 65535
+    def get_range_mid_left_back(self):
+        return self.sensor6, UNUSED_SENSOR
 
-    def get_range7(self):
-        return self.sensor7, 65535
+    def get_range_mid_right_back(self):
+        return self.sensor7, UNUSED_SENSOR
 
-    def get_range8(self):
-        return self.sensor8, 65535
+    def get_range_right_back(self):
+        return self.sensor8, UNUSED_SENSOR
+
+    def get_sensor1_range(self):
+        return self.execute(GET_SENSOR1_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor2_range(self):
+        return self.execute(GET_SENSOR2_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor3_range(self):
+        return self.execute(GET_SENSOR3_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor4_range(self):
+        return self.execute(GET_SENSOR4_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor5_range(self):
+        return self.execute(GET_SENSOR5_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor6_range(self):
+        return self.execute(GET_SENSOR6_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor7_range(self):
+        return self.execute(GET_SENSOR7_OPCODE).read(USHORT) , UNUSED_SENSOR
+
+    def get_sensor8_range(self):
+        return self.execute(GET_SENSOR8_OPCODE).read(USHORT) , UNUSED_SENSOR
 
     def is_ready(self):
         try:
@@ -102,4 +131,3 @@ if __name__ == "__main__":
     from setups.setup_serialtalks import *
 
     s = Sensors(manager, '/dev/tty.SLAB_USBtoUART')
-    s.subscribeSensors()
